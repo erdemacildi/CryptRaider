2e81f9c38426a18b068aae49a6cecac60157655b
erdemacildi <46825955+erdemacildi@users.noreply.github.com>
2022-09-29 10:43:38 +0300

    Secret Passage Door

--- Source/CryptRaider/Mover.cpp
+++ Source/CryptRaider/Mover.cpp
@@ -3,6 +3,8 @@
 
 #include "Mover.h"
 
+#include "Math/UnrealMathUtility.h"
+
 // Sets default values for this component's properties
 UMover::UMover()
 {
@@ -19,7 +21,7 @@ void UMover::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// ...
+	OriginalLocation = GetOwner()->GetActorLocation();
 	
 }
 
@@ -29,12 +31,24 @@ void UMover::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponent
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	AActor* Owner = GetOwner();
+	if (ShouldMove)
+	{
+		FVector CurrentLocation = GetOwner()->GetActorLocation();
+		FVector TargetLocation = OriginalLocation + MoveOffset;
+		float Speed = FVector::Distance(OriginalLocation,TargetLocation)/MoveTime;
+
+		FVector NewLocation = FMath::VInterpConstantTo(CurrentLocation, TargetLocation, DeltaTime, Speed);
+		GetOwner()->SetActorLocation(NewLocation);
+	}
+	
+	
+	
+
+	/*AActor* Owner = GetOwner();
 	FString OwnerName = Owner->GetActorNameOrLabel();
 	FVector OwnerLocation = Owner->GetActorLocation();
 	FString OwnerLocationString = OwnerLocation.ToCompactString();
 
-	UE_LOG(LogTemp , Display , TEXT("Mover Owner: %s with location %s") , *OwnerName , *OwnerLocationString);
-	// ...
+	UE_LOG(LogTemp , Display , TEXT("Mover Owner: %s with location %s") , *OwnerName , *OwnerLocationString);*/
 }
 
